e5a6e088e433c800b238d748620a82393e62938c
kowill <mob.koya@gmail.com>
2016-03-07 19:02:57 +0900

    change

---
+++ BigInteger/BigInteger.Tests/BigIntegerTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sample.Library;
+
+namespace BigIntgereger.Tests
+{
+    [TestClass]
+    public partial class BigIntgeregerTest
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Assert.AreEqual(new BigIntger(5) + new BigIntger(3), new BigIntger(8));
+            Assert.AreEqual(new BigIntger(5) + new BigIntger(8), new BigIntger(13));
+            Assert.AreEqual(new BigIntger(5) + new BigIntger(199), new BigIntger(204));
+        }
+
+        [TestMethod]
+        public void Subtract()
+        {
+            Assert.AreEqual(new BigIntger(5) - new BigIntger(3), new BigIntger(2));
+            Assert.AreEqual(new BigIntger(15) - new BigIntger(8), new BigIntger(7));
+            Assert.AreEqual(new BigIntger(100) - new BigIntger(3), new BigIntger(97));
+            Assert.AreEqual(new BigIntger(100) - new BigIntger(99), new BigIntger(1));
+        }
+
+        [TestMethod]
+        public void multiplication()
+        {
+            Assert.AreEqual(3 * new BigIntger(3), new BigIntger(9));
+            Assert.AreEqual(15 * new BigIntger(2), new BigIntger(30));
+        }
+    }
+}
---
+++ BigInteger/BigInteger/BigIntger.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Sample.Library
+{
+    public class BigIntger : IEquatable<BigIntger>
+    {
+        private string _bigInteger;
+        public BigIntger(int value)
+        {
+            _bigInteger = value.ToString();
+        }
+        public BigIntger(string value)
+        {
+            _bigInteger = value;
+        }
+
+        public string GetCharctor(int index)
+        {
+            return (0 <= index) && (index < _bigInteger.Length) ? _bigInteger[index].ToString() : "0";
+        }
+
+        public static BigIntger operator +(BigIntger x, BigIntger y)
+        {
+            var xLength = x.ToString().Length;
+            var yLength = y.ToString().Length;
+            var max = xLength < yLength ? yLength : xLength;
+
+            var up = false;
+            var sb = new StringBuilder();
+            for (int i = 0; (i < max) || up; i++)
+            {
+                var numX = 0;
+                int.TryParse(x.GetCharctor(xLength - i - 1), out numX);
+                var numY = 0;
+                int.TryParse(y.GetCharctor(yLength - i - 1), out numY);
+
+                var num = numX + numY;
+
+                if (up)
+                {
+                    num++;
+                    up = false;
+                }
+
+                if (9 < num)
+                {
+                    num -= 10;
+                    up = true;
+                }
+                sb.Insert(0, num.ToString());
+            }
+            return new BigIntger(sb.ToString());
+        }
+
+        public static BigIntger operator ++(BigIntger x)
+        {
+            var tmp = x + new BigIntger(1);
+            return tmp;
+        }
+
+        //xが大きい場合のみ
+        public static BigIntger operator -(BigIntger x, BigIntger y)
+        {
+            var xLength = x.ToString().Length;
+            var yLength = y.ToString().Length;
+            var max = xLength < yLength ? yLength : xLength;
+
+            var down = false;
+            var sb = new StringBuilder();
+            for (int i = 0; (i < max) || down; i++)
+            {
+                var numX = 0;
+                int.TryParse(x.GetCharctor(xLength - i - 1), out numX);
+                var numY = 0;
+                int.TryParse(y.GetCharctor(yLength - i - 1), out numY);
+
+                var num = numX - numY;
+
+                if (down)
+                {
+                    num--;
+                    down = false;
+                }
+
+                if (num < 0)
+                {
+                    num += 10;
+                    down = true;
+                }
+                sb.Insert(0, num.ToString());
+
+            }
+            var numText = string.Concat(sb.ToString().SkipWhile(c => c == '0'));
+            return new BigIntger(numText);
+        }
+
+        public static BigIntger operator *(int x, BigIntger y)
+        {
+            var result = new BigIntger(0);
+            for (int i = 0; i < x; i++)
+            {
+                result += y;
+            }
+            return result;
+        }
+
+        private bool IsZero()
+        {
+            return String.IsNullOrEmpty(_bigInteger) || (_bigInteger == "0");
+        }
+
+        public override string ToString()
+        {
+            return _bigInteger;
+        }
+
+        public bool Equals(BigIntger other)
+        {
+            return _bigInteger == other.ToString();
+        }
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as BigIntger);
+        }
+        public override int GetHashCode()
+        {
+            return _bigInteger.GetHashCode();
+        }
+    }
+}
